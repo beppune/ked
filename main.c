ff13d537a3e7bdc5cbd8c9aff4761f56d27b00e9
beppe <manzo.giuseppe@gmail.com>
2025-04-14 13:26:44 +0200

    ctrl-m

--- main.c
+++ main.c
@@ -15,7 +15,9 @@ void enable_raw() {
 	atexit(disable_raw);
 
 	struct termios raw = saved;
-	raw.c_lflag &= ~(ECHO | ICANON);
+	raw.c_cflag &= ~(IXON | ICRNL);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_lflag &= ~(ECHO | IEXTEN | ICANON | ISIG);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -26,9 +28,9 @@ int main() {
 	char c;
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q' ) {
 		if ( iscntrl(c) ) {
-			printf("%d\n", c);
+			printf("%d\r\n", c);
 		} else {
-			printf("%d ('%c')\n", c, c);
+			printf("%d ('%c')\r\n", c, c);
 		}
 
 	}
